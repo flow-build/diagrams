61c9947fc527f7a8fd09e642af1709f0c0003c57
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-08-15 20:18:09 -0300

    feat: add logger

---
+++ src/utils/logger.js
@@ -0,0 +1,21 @@
+const { createLogger, format, transports } = require('winston');
+
+const logger = createLogger({
+  level: 'info',
+  format: format.combine(
+    format.colorize(),
+    format.timestamp(),
+    format.printf((info) => `${info.timestamp} ${info.level} ${info.message}`)
+  ),
+  transports: [new transports.Console()],
+  exceptionHandlers: [
+    new transports.Console({
+      format: format.errors()
+    }),
+  ],
+  rejectionHandlers: [new transports.Console()]
+})
+
+module.exports = {
+  logger
+}
