07dde46488078ffd3041d5463a5d811cb4d123f7
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-08-15 20:20:45 -0300

    feat: add errorHandler middleware

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,14 @@
+module.exports = async (ctx, next) => {
+  try {
+    await next();
+  } catch(err) {
+    if (!err.statusCode) {
+      ctx.status = 400;
+      ctx.body = { message: err.message || 'Unknown Error'}
+    } else {
+      ctx.status = error.status;
+      ctx.body = { message: err.message || 'Internal Server Error'}
+    }
+    ctx.app.emit('error', err, ctx);
+  }
+}
