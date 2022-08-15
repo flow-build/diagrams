b31b33c4aad386b3b0bcbcd05863e2ad22a7f031
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-08-15 20:20:25 -0300

    feat: add token controller

---
+++ src/controllers/token.js
@@ -0,0 +1,25 @@
+const { v1: uuid } = require('uuid');
+const { createJWTToken } = require('../services/tokenGenerator');
+const { jwtSecret } = require('../utils/jwtSecret');
+
+const getToken = (ctx, next) => {
+  const secret = jwtSecret;
+
+  const body = ctx.request.body || {};
+  if (!body?.actor_id) {
+    body.actor_id = uuid();
+  }
+
+  const jwtToken = createJWTToken(body, secret);
+  ctx.status = 200;
+  ctx.body = { 
+    jwtToken,
+    payload: body
+  }
+
+  return next();
+}
+
+module.exports = {
+  getToken
+}
