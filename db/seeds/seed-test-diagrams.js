9f53c776a49b20c2b1f5f446f3ad0ece9fec0001
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-08-22 20:01:53 -0300

    refactor: rename diagram from seeds

--- db/seeds/seed-test-diagrams.js
+++ db/seeds/seed-test-diagrams.js
@@ -5,7 +5,7 @@ exports.seed = async function(knex) {
   await knex('diagrams').insert([
     {
       id: 'd655538b-95d3-4627-acaf-b391fdc25142', 
-      name: 'Test',
+      name: 'Example Diagram',
       diagram_xml: diagramSample,
       workflow_id: '7be513f4-98dc-43e2-8f3a-66e68a61aca8',
       user_id: '1'
