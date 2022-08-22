5bb37c1f385fdb0fa23001ecd374af645307c2e1
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-08-22 11:31:49 -0300

    feat: add duration and custom secret to JWT sign

--- src/controllers/token.js
+++ src/controllers/token.js
@@ -2,10 +2,11 @@ const { createJWTToken } = require('../services/tokenGenerator');
 const { jwtSecret } = require('../utils/jwtSecret');
 
 const getToken = (ctx, next) => {
-  const secret = jwtSecret;
+  const secret = ctx.get("x-secret") || jwtSecret;
+  const duration = parseInt(ctx.get("x-duration")) || 3600;
 
-  const jwtToken = createJWTToken(secret);
-  ctx.status = 200;
+  const jwtToken = createJWTToken(secret, duration);
+  ctx.status = 200; 
   ctx.body = { 
     jwtToken
   }
--- src/services/tokenGenerator.js
+++ src/services/tokenGenerator.js
@@ -1,9 +1,16 @@
 const rs = require('jsrsasign');
 
-const createJWTToken = (secret) => {
+const createJWTToken = (secret, duration) => {
   const jwtHeader = { alg: 'HS256', typ: 'JWT' };
 
-  return rs.KJUR.jws.JWS.sign('HS256', jwtHeader, {}, { utf8: secret })
+  const tNow = rs.KJUR.jws.IntDate.get('now');
+  const tEnd = tNow + duration;
+  const payload = {
+    iat: tNow,
+    exp: tEnd
+  }
+
+  return rs.KJUR.jws.JWS.sign('HS256', jwtHeader, payload, { utf8: secret });
 }
 
 module.exports = {
