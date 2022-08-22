5e71e9874c9408d92612b90e65f7a3d7c370001b
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-08-22 19:57:26 -0300

    fix: adjust errorHandler

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,9 +5,20 @@ module.exports = async (ctx, next) => {
     await next();
   } catch(err) {
     logger.error(err);
-
-    ctx.status = err.status || err.statusCode || 500;
-    ctx.body = { message: err.message || 'Internal Server Error'}
+    
+    if (err.error) {
+      ctx.status = err.status || err.statusCode || 500;
+      ctx.body = { 
+        message: err.message || 'Internal Server Error',
+        error: err.error
+      }
+    } else {
+      ctx.status = err.status || err.statusCode || 500;
+      ctx.body = { 
+      message: err.message || 'Internal Server Error'
+    }
+    
+    }
     ctx.app.emit('error', err, ctx);
   }
 }
