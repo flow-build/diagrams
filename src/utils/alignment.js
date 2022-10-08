11c093ffb39934c65ddaf64eebd2466ae226f2f7
Pedro Pereira Assis <pedro.assis@fdte.io>
2022-10-08 12:33:49 -0300

    feat: adjust util to accept flowNodes

--- src/utils/alignment.js
+++ src/utils/alignment.js
@@ -5,18 +5,40 @@ const checkAlignment = async (bp, diagram) => {
   const bpClean = bp.blueprint_spec.nodes.map((node) => {
     return { id: node.id, next: node.next, type: node.type.toLowerCase() }
   });
-  
+  const bpOrdered = bpClean.sort((a, b) =>  a.id > b.id ? -1 : 0);
+  console.log(bpClean)
+
   const bpDiagram = await buildBlueprintFromBpmn(diagram);
 
   const bpDiagramClean = bpDiagram.blueprint.blueprint_spec.nodes.map((node) => {
-    const id = node.name.replaceAll(' ', '-');
-    const nextId = bpDiagram.blueprint.blueprint_spec.nodes.find((nextNode) => nextNode.id === node.next);
-    const next = nextId === undefined ? null : nextId.name.replaceAll(' ', '-');
+    const id = node?.name.replaceAll(' ', '-');
+    let next;
+    if (node.next && typeof node.next !== 'object') {
+      const nextId = bpDiagram.blueprint.blueprint_spec.nodes.find((nextNode) => nextNode.id === node.next);
+      next = nextId?.name.replaceAll(' ', '-');
+    } else if (node.next) {
+      next = {};
+      for (const [key, value] of Object.entries(node.next)) {
+        const nextId = bpDiagram.blueprint.blueprint_spec.nodes.find((nextNode) => nextNode.id === value);
+        if (key.includes(', ')) {
+          const keys = key.split(', ');
+          keys.forEach(key => next[key] = nextId?.name.replaceAll(' ', '-'));
+        } else if (key.includes(' ,')) {
+          const keys = key.split(' ,');
+          keys.forEach(key => next[key] = nextId?.name.replaceAll(' ', '-'));
+        } else {
+          next[key] = nextId?.name.replaceAll(' ', '-');
+        }
+      }
+    } else {
+      next = null;
+    }
     return { id, next, type: node.type.toLowerCase() }
   });
-  console.log(bpDiagramClean)
-
-  return _.isEqual(bpClean, bpDiagramClean);
+  const bpDiagramOrdered = bpDiagramClean.sort((a, b) =>  a.id > b.id ? -1 : 0);
+  console.log(bpDiagramOrdered)
+  console.log(_.isEqual(bpOrdered, bpDiagramOrdered))
+  return _.isEqual(bpOrdered, bpDiagramOrdered);
 }
 
 module.exports = {
