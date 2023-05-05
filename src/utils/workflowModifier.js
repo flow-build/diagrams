69010b122d3c86564cde6008bb8345444727c3ea
Pedro Pereira Assis <pedro.assis@fdte.io>
2023-05-05 17:06:47 -0300

    refactor: refactor workflowModifier & workflow validator

--- src/utils/workflowModifier.js
+++ src/utils/workflowModifier.js
@@ -1,29 +1,36 @@
 const _ = require('lodash');
 
-async function removeNodesByCategory(blueprint_spec, category) {
-  for (const node of blueprint_spec.nodes) {
-    if (node.next && typeof node.next !== 'object') {
-      let nextNode = node;
-      do {
-        nextNode = blueprint_spec.nodes.find((_node) => _node.id === nextNode.next);
-      } while (nextNode?.category?.toLowerCase() === category);
-      node.next = nextNode.id;
-    } else if (node.next) {
-      for (const [nextKey, nextValue] of _.uniq(Object.entries(node.next))) {
-        let nextNode = blueprint_spec.nodes.find((_node) => _node.id === nextValue);
-        while (nextNode?.category?.toLowerCase() === category) {
-          nextNode = blueprint_spec.nodes.find((_node) => _node.id === nextNode.next);
-        }
-        if (nextNode.id === node.id) {
-          delete node.next[nextKey];
-        } else {
-          node.next[nextKey] = nextNode.id;
-        }
-      }
+function getNextExcludingCategory(nodes, next, category) {
+  nodes.map((myNode) => {
+    if (next === myNode.id && myNode?.category?.toLowerCase() === category) {
+      next = myNode.next;
     }
-  }
-  blueprint_spec.nodes = blueprint_spec.nodes.filter((node) => node?.category?.toLowerCase() !== category);
-  return blueprint_spec;
+  });
+  return next;
+}
+
+async function removeNodesByCategory(blueprint_spec, category) {
+  let blueprint = _.cloneDeep(blueprint_spec);
+  const nodes = [...blueprint_spec.nodes];
+  const filteredNodes = nodes.filter((node) => node?.category?.toLowerCase() !== category);
+
+  blueprint.nodes = filteredNodes.map((node) => {
+    if (!node.next) {
+      return node;
+    }
+    if (typeof node.next === 'object') {
+      _.uniq(Object.entries(node.next)).map(([nextKey, nextValue]) => {
+        node.next[nextKey] = getNextExcludingCategory(nodes, nextValue, category);
+        if (node.id === node.next[nextKey]) {
+          delete node.next[nextKey];
+        }
+      });
+      return node;
+    }
+    node.next = getNextExcludingCategory(nodes, node.next, category);
+    return node;
+  });
+  return blueprint;
 }
 
 module.exports = {
