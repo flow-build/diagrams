6052390f0c4cdd9e6c2b735507bf37c801fddfc8
imagure <ricardo.imagure@gmail.com>
2023-05-11 12:39:48 -0300

    refactor: updates tokenGenerator to comply with input body change

--- src/controllers/token.js
+++ src/controllers/token.js
@@ -5,12 +5,12 @@ const { logger } = require('../utils/logger');
 const getToken = (ctx, next) => {
   logger.debug('getToken controller called');
 
-  const user_id = ctx.request.body?.user_id || null;
-  if (user_id) {
+  const userId = ctx.request.body?.userId || null;
+  if (userId) {
     const secret = ctx.get('x-secret') || jwtSecret;
     const duration = parseInt(ctx.get('x-duration')) || 3600;
 
-    const jwtToken = createJWTToken(secret, duration, user_id);
+    const jwtToken = createJWTToken(secret, duration, userId);
     ctx.status = 200;
     ctx.body = {
       jwtToken,
@@ -18,7 +18,7 @@ const getToken = (ctx, next) => {
 
     return next();
   }
-  throw Error('No user_id informed');
+  throw Error('No userId informed');
 };
 
 module.exports = {
